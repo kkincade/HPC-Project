b64f011aea9603ddbfc592e4de866cb19e62d4e9
Kameron Kincade <kameron.kincade@me.com>
2014-11-10 23:52:18 -0700

    Added draft of progress update

--- src/test.cu
+++ src/test.cu
@@ -1,68 +1,67 @@
-#include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
 #include <string>
-#include <map>
-#include <thrust/host_vector.h>
- 
-// Kernel that executes on the CUDA device
-__global__ void square_array(int *a, int N) {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if (idx<N) a[idx] = idx;
-}
- 
-// main routine that executes on the host
-int main(void) {
 
-	int *thread_ids_h, *thread_ids_d;  // Pointer to host & device arrays
-	const int N = 10;  // Number of elements in arrays
-	size_t size = N * sizeof(int);
+using namespace std;
 
-	thread_ids_h = (int *)malloc(size); // Allocate array on host
+// const int NUM_BLOCKS = 4;
+// const int THREADS_PER_BLOCK = 100;
 
-	cudaMalloc((void **) &thread_ids_d, size); // Allocate array on device
+// kernel which copies data from d_array to destinationArray  
+__global__ void EvaluateFSM(int* d_fsm, int* threadResultStates, int* d_input, int* d_startStates, size_t pitch, int NUM_SYMBOLS, int NUM_STATES) {  
+  int currentState = 0;
 
-	// Initialize host array and copy it to CUDA device
-	for (int i=0; i<N; i++) thread_ids_h[i] = (int)i;
-
-	cudaMemcpy(thread_ids_d, thread_ids_h, size, cudaMemcpyHostToDevice);
-
-	// Do calculation on device:
-	int block_size = 4;
-	int n_blocks = N/block_size + (N%block_size == 0 ? 0:1);
-	square_array <<< n_blocks, block_size >>> (thread_ids_d, N);
-
-	// Retrieve result from device and store it in host array
-	cudaMemcpy(thread_ids_h, thread_ids_d, size, cudaMemcpyDeviceToHost);
-
-	// Print results
-	for (int i=0; i<N; i++) printf("%i %i\n", i, thread_ids_h[i]);
-
-	// Cleanup
-	free(thread_ids_h); cudaFree(thread_ids_d);
+  for (int i = 0; i < 8; i++) { 
+    int symbol = d_input[i];
+    int* rowData = (int*) (((char*)d_fsm) + (currentState * pitch));
+    currentState = rowData[symbol];
+    threadResultStates[i] = currentState;
+  }  
 }
 
-// using namespace std;
 
-// #define NUM_BLOCKS 1
-// #define NUM_THREADS 256
+int main(int argc, char** argv) {  
 
-// // 1. Access the FSM from global memory. 
-// // 2. Use the ThreadID to figure out the start state and the input characters. 
-// // 3. Evaulate the FSM.
-// // 4. Save resulting state in global memory.
-// __global__ static void evaluate_fsm(int *threadIDs[], int length) {
+	// FSM for detecting three ones in a row  
+	const int NUM_STATES = 4; 
+  const int NUM_SYMBOLS = 2;
+  int fsm[NUM_STATES][NUM_SYMBOLS] = { {0, 1}, {0, 2}, {0, 3}, {3, 3} };
+  int *d_fsm;
 
-// 	// Get thread and block IDs
-// 	// const int tid = threadIdx.x;
-//     // const int bid = blockIdx.x;
+  int input[10] = { 0, 0, 0, 1, 1, 0, 1, 1, 1, 1 };
+  int *d_input;
 
+  int startStates[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  int *d_startStates;
 
+  int h_threadResultStates[NUM_SYMBOLS*NUM_STATES] = { -1, -1, -1, -1, -1, -1, -1, -1 };
+  int* d_threadResultStates;
 
-// 	// for (int i = 0; i < NUM_THREADS; i++) {
-// 	// 	if ((*threadIDs)[i] == -1) {
-// 	// 		(*threadIDs)[i] = tid;
-// 	// 	}
-// 	// }
-    
+  // The pitch value assigned by cudaMallocPitch  
+  // (which ensures correct data structure alignment)  
+  size_t pitch;   
+
+	// Allocate the device memory
+  cudaMallocPitch(&d_fsm, &pitch, NUM_SYMBOLS * sizeof(int), NUM_STATES);   
+  cudaMalloc(&d_threadResultStates, (NUM_SYMBOLS * NUM_STATES * sizeof(int))); 
+  cudaMalloc(&d_input, 10); 
+  cudaMalloc(&d_startStates, 10);
+
+  // Copy host memory to device memory
+  cudaMemcpy2D(d_fsm, pitch, fsm, (NUM_SYMBOLS * sizeof(int)), (NUM_SYMBOLS * sizeof(int)), NUM_STATES, cudaMemcpyHostToDevice);  
+  cudaMemcpy(d_threadResultStates, h_threadResultStates, NUM_STATES*NUM_SYMBOLS, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_input, input, 10, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_startStates, startStates, 10, cudaMemcpyHostToDevice);
+
+  // Call the kernel
+  EvaluateFSM<<<100, 512>>>(d_fsm, d_threadResultStates, d_input, d_startStates, pitch, NUM_SYMBOLS, NUM_STATES);
+
+  // Copy the data back to the host memory  
+  cudaMemcpy(h_threadResultStates, d_threadResultStates, (NUM_SYMBOLS * NUM_STATES * sizeof(int)), cudaMemcpyDeviceToHost);  
+
+  // Print out the values (all the values are 123)  
+  for (int i = 0 ; i < NUM_STATES * NUM_SYMBOLS; i++) { 
+      cout << "h_threadResultStates[" << i << "] = " << h_threadResultStates[i] << endl;   
+  }  
+}  
